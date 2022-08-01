10be4392a0f7fcac72894040245c2998c63da11d
pkamano <p.amano1225@protonmail.com>
2022-08-01 08:45:53 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,38 +15,132 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+// const people = [
+//   { name: "ford prefect", occupation: "a hitchhiker" },
+//   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+//   { name: "arthur dent", occupation: "a radio employee" }
+// ]
+// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("capitalPeople", () => {
+  const people = [
+    { name: "ford prefect", occupation: "a hitchhiker" },
+    { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+    { name: "arthur dent", occupation: "a radio employee" }
+  ]
+}) 
+it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+  expect(capitalPeople(people)).toEqual("[Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee.]")
+  });
+
+ 
+// ReferenceError: capitalPeople is not defined
+
+// b) Create the function that makes the test pass.
+// First I would destructure the object since only the first letters of the names key value pairs are being capitalized. Then .toUppercase the first letter (index[0]) of each word. There's about 1.5 weeks of attempts so I did not put all those on there. 
+//Parameters: name and occupation, people
+//Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+const name = people.map(value => value.name)
+console.log(name)
+// [ 'ford prefect', 'zaphod beeblebrox', 'arthur dent' ]
+const occupation = people.map(value => value.occupation)
+console.log(occupation)
+// [ 'a hitchhiker', 'president of the galaxy', 'a radio employee' ]
 
+// The issue I had was I was so stuck on destructuring the object but I did not know how to put it back together. I used string interpolation to get the sentence back together but trying to get the second word to capitalize was where I was really stuck. I am pretty happy that I used HOF because I was scared to even attempt it at first. 
 
-// b) Create the function that makes the test pass.
+const capitalPeople = (array) => {
+    let singleWords = array.map(value => {
+        return value[0].toUpperCase() + value.substring(1) + ` is ${occupation.shift()}.`
+    })
+    return singleWords
+    }
+
+console.log(capitalPeople(name))
+// [
+//     'Ford prefect is a hitchhiker.',
+//     'Zaphod beeblebrox is president of the galaxy.',
+//     'Arthur dent is a radio employee.'
+//   ]
+//I also have to figure out how call the class and not just the object. 
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
+// const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+// Expected output: [ 2, 0, -1, 0 ]
+// const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+// Expected output: [ 2, 1, -1 ]
+describe("integersOnly", () => {
+  const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(integersOnly(hodgepodge1)).toEqual([2, 0, -1, 0]);
+    expect(integersOnly(hodgepodge2)).toEqual([2, 1, -1])
+  });
+});
+
+// ReferenceError: integersOnly is not defined
+
+
+// b) Create the function that makes the test pass.
+
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+// Since the expected output is integers only, I knew I would have to filter out the integers. However, when I tried to use the modulo (thinking it was the same as remainder) operator it did not work. Led me to research the difference between modulo and remainder. Using .filter, I filtered out the integers from the array and then .map over the integers while getting their remainder when those integers were divided by three. Not going to lie, I was pretty happy that I was able to figure out how to use the HOF and that it worked!
+//Parameters: const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false], const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+// Expected output: [ 2, 0, -1, 0 ], [ 2, 1, -1 ]
+const integersOnly = (array) => {
+  return array.filter(value => typeof value === 'number').map(value => value %= 3)
+}
 
-// b) Create the function that makes the test pass.
-
+ console.log(integersOnly(hodgepodge1))
+ console.log(integersOnly(hodgepodge2))
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
+const cubeAndSum1 = [2, 3, 4] 
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("sumCubed", () => {
+  const cubeAndSum1 = [2, 3, 4]
+  const cubeAndSum2 = [0, 5, 10]
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(sumCubed(cubeAndSum1)).toEqual(99);
+    expect(sumCubed(cubeAndSum2)).toEqual(1125)
+  });
+});
+
+// ReferenceError: sumCubed is not defined
 
 // b) Create the function that makes the test pass.
+// Declare a function: use .reduce()to calculate the sum and then cube the sum using **3; use foreach to cube it first then add together. <-- That was my initial thought process. If I just stuck with that thought process, my expected output was incorrect. So I did some calculating on the side and realized that each individual value has to be cubed (** 3) first. I used .map to iterate over each value and cube it. When the array came back [8, 27, 64] then I had to research how to add the values in the array to one another. An issue I had was not declaring sum as 0. I thought I could just put the "newArray" as what sum would equal. Once I put sum back to 0 and followed the example for loop, I was able to get the correct answer. 
+
+// Set parameters: const cubeAndSum1 = [2, 3, 4], const cubeAndSum2 = [0, 5, 10]
+// Expected Output: 99, 1125
+
+
+const sumCubed = (array) => { 
+  let newArray = array.map(value => value ** 3);
+  let sum = 0
+  for(let i=0; i<newArray.length; i++){
+      sum += newArray[i]
+  }
+  return sum
+}
+
+console.log(sumCubed(cubeAndSum1))
+console.log(sumCubed(cubeAndSum2))
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,63 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: In order to fix this, you would have to generate a migration to add a column to the Student model. The name of the foreign key is cohort_id. The foreign key would be on the Student model. Students belong_to a cohort so each student would have a single cohort id versus each cohort instance having multiple student ids.
 
-Researched answer:
+Researched answer: In order to fix this, you would have to generate a migration to add a column to the Student model. The add_column with the specified parameters (name of the table, what name of the new column will be and the data type of the new column) should also be entered in the migration file that was made in the code editor. Then the migrate command needs to be run again in order to implement and save the changes. The standard naming convention of the foreign key is modelname_id so in this case it would be "cohort_id". The foreign key always goes to the belongs_to side so in this case, it would go on the Student model. 
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: The restful routes that always need to be passed params are show, create, update, and delete. Show needs a param in the event that an entry or specified entries need to be viewed for clarification or editing purposes. Create needs a param because there needs to be specific data passed to create a new instance. For example, if I was adding a new entry into a table about dogs that asked for the breed and age, whenever I create a new instance, those two params need to be passed to become a valid instance. Update needs params so that if the information is update, there is continuity in data type and content. For example, when updating an instance in my dog table, I would not put a name of a dog since the params are asking for breed and age. Delete also needs params for all the aforementioned reasons and as a double check to make sure the right data is getting deleted. 
 
-Researched answer:
+Researched answer: Params make the controller more dynamic and uses the routes to pass information versus the URL. The restful routes that always need to be passed params are show, create, update, and delete. Show needs a param in the event that an entry or specified entries need to be viewed for clarification or editing purposes. Create needs a param because there needs to be specific data passed to create a new instance. For example, if I was adding a new entry into a table about dogs that asked for the breed and age, whenever I create a new instance, those two params need to be passed to become a valid instance. Update needs params so that if the information is update, there is continuity in data type and content. For example, when updating an instance in my dog table, I would not put a name of a dog since the params are asking for breed and age. Delete also needs params for all the aforementioned reasons and as a double check to make sure the right data is getting deleted.
 
 3. Name three rails generator commands. What is created by each?
 
-Your answer:
+Your answer: The three rails generator commands are $ rails generate model, $ rails generate resource, and $ rails generate migration. $ rails generate resource builds the model and all the RESTful routes associate with the model. Instead of building a resource, $ rails generate model will create a new model without the RESTful routes. $rails generate migration will create a specific migration. For example, if I wanted to add a column, I would input $ rails generate migration add_column and then the appropriate accompanying information. 
 
-Researched answer:
+Researched answer: $ rails generate model creates a new model with the column name(s) and the data type for each column created. $ rails generate migration action_name allows changes to be made to the database. $ rails generate resource creates a model with the columns, data types, controller, view folder and RESTful routes so those do not have to be created individually in the text editor. 
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET" location: /students
+This action would display all the students (instances) in the table. 
 
 action: "POST" location: /students
+This action would create a new instance in the table given the appropriate parameters are provided. 
 
 action: "GET" location: /students/new
+This action would display the form of putting in a new instance in the table.
 
 action: "GET" location: /students/2
+This action would show the instance with the primary key (unique) id:2 in the students table.
 
 action: "GET" location: /students/2/edit
+This action would display the form of editing instance with the primary key (unique) id:2 in the students table.
 
 action: "PATCH" location: /students/2
+This action would invoke any edits that were made and update the instance of primary key (unique) id:2 in the table.
 
 action: "DELETE" location: /students/2
+This action would delete the instance with the primary key (unique) id:2 from the table. 
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+1) As a user, I want an application on my phone, desktop, tablet that can be used to manage a to-do list so I can use it on any device. 
+
+2) As a user, I want to be able to personalize my profile with pictures and other elements so if I'm using a shared device my list items are seperate. 
+
+3) As a user, I want to be able to create sub-lists/categories for other individuals in my family that I have to keep track of their tasks. 
+
+4) As a user, I want to be able to sync my email, texts, and default calendar applications with the app so I can easily consolidate information with less chance of error. 
+
+5) As a user, I want to be able to create, edit and delete tasks complete with outside contact information.
+
+6) As a user, I want to be able to recover an entry within a certain time period just in case the entry was deleted by mistake. 
+
+7) As a user, I want to be able to send entries and entire lists by text or email and upload a backup file to a cloud resource in case of any mishaps regarding my devices.
+
+8) As a user, I want to be able to have the option to password protect and encrypt each individual list so I can add personal information and to protect against physical theft.
+
+9) As a user, I want to be able to have the list formatted so I can print it if I need/ want a physical copy. 
+
+10) As a user, I want to be able to select and duplicate entries to different days, weeks, months, years for reoccuring events such as annual car registration.
